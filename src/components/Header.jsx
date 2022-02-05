24db53881ca9768864ef604eee5111ffead3cc85
joyadeep <joyadeep.limbu@gmail.com>
2022-02-05 09:42:13 +0545

    migrated from ref to state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 export const Header = () => {
   return <>
-    <div className='bg-indigo-300 relative h-20 flex justify-between items-center z-40'>
-        <Link to="/"><div className='text-7xl font-semibold text-white pl-3'>N<span className='text-[20px] font-normal '>flix</span></div></Link>
+    <div className='bg-gradient-to-r from-gray-700 to-blue-400 relative h-16 flex justify-between items-center z-40'>
+        <Link to="/"><div className='text-6xl font-semibold text-white pl-3'>N<span className='text-[20px] font-normal '>flix</span></div></Link>
  
         <div className='px-2'>
             <input className='bg-green-700 cursor-pointer text-white  h-10 px-1 text-xl rounded-md font-medium hover:bg-green-800 active:bg-green-600' type="button" value="Login" />
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,21 +2,25 @@ import React, { useRef } from 'react';
 import {useDispatch} from 'react-redux';
 import {search_movie,cleanup_list} from '../Redux/Action/action';
 import {useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+
 
 export const Landing = () => {
-    const tosearch=useRef();
+  const [tosearch,setTosearch]=useState("");
     const dispatch=useDispatch();
     const navigate=useNavigate();
     const changeRoute=()=>{
         navigate("/movies")
     }
+   
   return <>
-    <div className='flex h-screen absolute top-0 w-screen  text-white items-center justify-center z-20  bg-hero'>
-    <input type="text" ref={tosearch} placeholder="Search here..." className='pl-4 text-2xl rounded-md w-1/2 h-14 outline-none text-black'/>
-    <button type='submit' className='h-14 ml-3 bg-purple-500 px-4 rounded-md text-2xl hover:bg-purple-400' onClick={()=>{
+    <div className='flex h-screen absolute top-0 w-screen  text-white items-center justify-center z-20  bg-hero object-cover'>
+    <input type="text" value={tosearch} onChange={(e)=>{setTosearch(e.target.value)}}  placeholder="Search here..." className='pl-4 text-2xl rounded-md w-1/2 h-14 outline-none text-black'/>
+    <button disabled={!tosearch}  type='submit' className='h-14 ml-3 bg-purple-500 px-4 rounded-md text-2xl hover:bg-purple-400' onClick={()=>{
         dispatch(cleanup_list());
-        dispatch(search_movie(tosearch.current.value));
-        changeRoute();
+        dispatch(search_movie(tosearch));
+        changeRoute()
+        
     }}>Search</button>
     </div>
   </>;
